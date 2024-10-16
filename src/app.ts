2d6a5386a7d3aeadc0367a60595d7f8e6be6490b
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2024-10-16 22:17:09 +0600

    helmet added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -11,10 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(helmet());
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the server' });
 });
+
 
 
 app.listen(PORT, () => {
